3290f3d6309ef90546888fa3364cb17f6eda07ed
Mireille Anwar <mireille.anwar@hotmail.com>
2014-05-09 14:29:23 -0400

    test

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,3 +1,5 @@
+echo "set"
+Set
 echo "xcopy %SourcesDirectory%\..\bin\CoreTweet.dll %SourcesDirectory%\CoreTweet\bin\Debug\"
 xcopy "%SourcesDirectory%\..\bin\CoreTweet.dll" "%SourcesDirectory%\CoreTweet\bin\Debug\" /S 
 
