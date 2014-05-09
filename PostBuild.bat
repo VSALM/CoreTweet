e2aee5ee9b95906338b005ceaf87974204c4cac3
Mireille Anwar <mireille.anwar@hotmail.com>
2014-05-09 13:52:49 -0400

    test

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,5 +1,3 @@
 echo "xcopy"
 xcopy ".\..\bin\CoreTweet.dll" ".\CoreTweet\bin\Debug\" /S /Y
 
-echo "nuget"
-"$(SolutionDir)\tools\NuGet.exe" pack "$(ProjectDir)CoreTweet.csproj" -BasePath "$(TargetDir)." -verbosity detailed -OutputDirectory "$(TargetDir)."
