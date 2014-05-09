4ad4cf9a5cf7d7e8fd9e9b61936a11373a08599a
Mireille Anwar <mireille.anwar@hotmail.com>
2014-05-09 14:42:56 -0400

    test

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,7 +1,7 @@
-echo "set"
-Set
 echo "xcopy %TF_BUILD_BINARIESDIRECTORY%\CoreTweet.dll %TF_BUILD_SOURCESDIRECTORY%\CoreTweet\bin\Debug\"
 xcopy "%TF_BUILD_BINARIESDIRECTORY%\CoreTweet.dll" "%TF_BUILD_SOURCESDIRECTORY%\CoreTweet\bin\Debug\" /S 
+echo "dir"
+dir %TF_BUILD_SOURCESDIRECTORY%\tools
 echo "nuget"
 "%TF_BUILD_SOURCESDIRECTORY%\tools\NuGet.exe" pack "$(ProjectDir)CoreTweet.csproj" -BasePath "$(TargetDir)." -verbosity detailed -OutputDirectory "$(TargetDir)."
 
