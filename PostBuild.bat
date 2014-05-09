bbd90f89aaa0e0ca133099e7e217dc9f0053b2c1
Mireille Anwar <mireille.anwar@hotmail.com>
2014-05-09 13:58:30 -0400

    test

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,3 +1,3 @@
-echo "xcopy"
-xcopy "$(SolutionDir)\..\bin\CoreTweet.dll" "$(SolutionDir)\CoreTweet\bin\Debug\" /S /Y
+echo "xcopy $(SolutionDir)\..\bin\CoreTweet.dll $(SolutionDir)\CoreTweet\bin\Debug\"
+xcopy "$(SolutionDir)\..\bin\CoreTweet.dll" "$(SolutionDir)\CoreTweet\bin\Debug\" /S 
 
